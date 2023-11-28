9cf8e89c825b53b3c0a02a21dfb75c77d7152cd7
Arion <Arion@DESKTOP-9BU80RS>
2023-11-28 18:25:11 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿//сложный 19
+Console.Write("Введите текст: ");
+char[] s1 = Console.ReadLine().ToCharArray();
+string s2 = "";
+int i, L;
+L = s1.Length;
+for (i = 0; i < L; i++)
+{
+    s2 = s2 + s1[L - i - 1];
+}
+Console.WriteLine("s1={0}, s2={1}", s1, s2);
+Console.ReadKey();
